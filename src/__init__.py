c53c0d0605f70497fd7e3f0224c333133ba26d4a
cueki <github2@madisonlovett.dev>
2026-01-03 10:58:55 -0800

    bump version

--- src/__init__.py
+++ src/__init__.py
@@ -13,7 +13,6 @@ from .vpk import VPKFile, VPKDirectoryEntry
 from .pcf import PCFFile, PCFElement
 from .constants import PCFVersion, AttributeType
 
-__version__ = "1.0.3"
 __all__ = [
     "VPKFile", 
     "VPKDirectoryEntry",
